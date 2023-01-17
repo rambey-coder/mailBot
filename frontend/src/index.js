0703dd6f097918a16b33f878b5cdb9e095e452d4
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-01-17 07:32:55 +0100

    feat: added router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ContextProvider from './context';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <Router>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </Router>
   </React.StrictMode>
 );
