ecdfe0df7f2743ae54c982fda195054cc9f67de0
rambey-coder <olayiwolaagboolaakaanji@gmail>
2023-01-19 06:57:54 +0100

    fix: sidebar styling issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home/Home'
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <>
+    <div className='mailbot-container'>
       <Sidebar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+      <div className='mailbot-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className="header">
+        <h3>Verify Email</h3>
+        <hr />
+      </div>
+    </div>
+  )
+}
+
+export default Home
